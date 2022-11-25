5573c7a95cbc2dc88f5fd03e4c578f50d0523c35
ManuelCamilo <manuelcamilo16@gmail.com>
2022-11-24 21:55:20 -0300

    simplificamos funciones dentro de cartWidget, navbar.jsx. quitamos contenedores y cambiamos etiquetas reemplazables con otra distribución en layout.

--- src/components/Categorias/categorias.jsx
+++ src/components/Categorias/categorias.jsx
@@ -2,22 +2,12 @@ import './categorias.css';
 
 const Categorias = () => {
     return (
-    <div className="collapse navbar-collapse">
-      <ul className="navbar-nav">
-        <li className="nav-item">
-        <button className="nav-link botonesNavBar" aria-current="page" > Juegos </button>
-        </li>
-        <li className="nav-item">
-        <button className="nav-link botonesNavBar" aria-current="page" > Consolas </button>
-        </li>
-        <li className="nav-item">
-        <button className="nav-link botonesNavBar" aria-current="page" > Notebooks </button>
-        </li>
-        <li className="nav-item">
-        <button className="nav-link botonesNavBar" aria-current="page" > Merch </button>
-        </li>
-      </ul>
-    </div>
+      <div className="navbar-nav">
+        <button className="nav-item nav-link botonesNavBar" aria-current="page" > Juegos </button>
+        <button className="nav-item nav-link botonesNavBar" aria-current="page" > Consolas </button>
+        <button className="nav-item nav-link botonesNavBar" aria-current="page" > Notebooks </button>
+        <button className="nav-item nav-link botonesNavBar" aria-current="page" > Merch </button>
+      </div>
     );
 }
 
--- src/components/NavBar/navbar.jsx
+++ src/components/NavBar/navbar.jsx
@@ -11,7 +11,9 @@ const Navbar = () => {
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                    <span className="navbar-toggler-icon" />
                 </button>
+                <div className="collapse navbar-collapse">
                 <Categorias/>
+                </div>
                 <CartWidget/>
             </div>
         </nav>
