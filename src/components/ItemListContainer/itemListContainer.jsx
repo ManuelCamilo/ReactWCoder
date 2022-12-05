fc759cc0fb4057c86c5baa4a6e1372eaeb92262f
ManuelCamilo <manuelcamilo16@gmail.com>
2022-12-05 14:23:43 -0300

    itemListContainer Check-

--- src/components/ItemListContainer/itemListContainer.jsx
+++ src/components/ItemListContainer/itemListContainer.jsx
@@ -1,10 +1,22 @@
+import { useState, useEffect} from "react";
 import ItemList from "../ItemList/ItemList";
 import consultarBDD from '../../assets/funciones.js'
 const ItemListContainer = ({greeting}) => {
+    const [productos,setProductos] = useState([]);
+    useEffect(() => {
+        consultarBDD().then(productList=> {
+            const cardProductos = ItemList({productList})
+            setProductos(cardProductos)
+        })
+    }, []);
+    console.log(productos)
 
-    consultarBDD().then(productos=>console.log(productos))
+
+    
     return (
-        <p>{greeting}</p>
+        <div className="row cardProductos">
+            {productos}
+        </div>
     );
 }
 
--- src/components/item/item.jsx
+++ src/components/item/item.jsx
@@ -1,8 +1,19 @@
 //card
 const Item = ({producto}) => {
+    console.log(producto)
     return (
         <>
-            {producto}
+            <div className="card cardProducto">
+                <img src={`../img/${producto.img}`} className="card-img-top" alt="al parecer nada..." />
+                    <div className="card-body">
+                    <h5 className="card-title">{producto.nombre}</h5>
+                    <p className="card-text">{producto.marca}</p>
+                    <p className="card-text">{producto.precio}</p>
+                    <button className="btn btn-dark">ver producto</button>
+       
+    </div>
+</div>
+
         </>
     );
 }
